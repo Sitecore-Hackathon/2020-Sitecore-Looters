64320fe07314c7401447ef8bfd171872cf7abc67
mshaltaf <mshaltaf@tanasuk.com>
2020-02-29 10:35:37 +0200

    Adding Content management To Foundation

---
+++ src/Foundation/ContentManagement/Models/GlassBase.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Boilerplate.Foundation.ContentManagement.Models
+{
+    using System;
+    using Glass.Mapper.Sc.Configuration;
+    using Glass.Mapper.Sc.Configuration.Attributes;
+
+    namespace SaudiAirlines.Foundation.Core.BaseClasses
+    {
+        // Glass base class
+        public abstract class GlassBase
+        {
+            /// <summary>
+            /// Get or set Name property
+            /// </summary>
+            [SitecoreInfo(SitecoreInfoType.Name)]
+            public virtual string Name { get; set; }
+
+            /// <summary>
+            /// Get or set Id property
+            /// </summary>
+            [SitecoreId]
+            public virtual Guid Id { get; set; }
+
+            /// <summary>
+            /// Get or Set the Language Property
+            /// </summary>
+            [SitecoreInfo(SitecoreInfoType.Language)]
+            public virtual string Language { get; set; }
+
+            /// <summary>
+            /// Get or set DisplayName property
+            /// </summary>
+            [SitecoreInfo(SitecoreInfoType.DisplayName)]
+            public virtual string DisplayName { get; set; }
+
+            /// <summary>
+            /// Template Id
+            /// </summary>
+            [SitecoreInfo(SitecoreInfoType.TemplateId)]
+            public virtual Guid TemplateId { get; set; }
+        }
+
+        // Glass parent class
+        public class GlassParent
+        {
+            /// <summary>
+            /// Get or set Id property
+            /// </summary>
+            [SitecoreId]
+            public virtual Guid Id { get; set; }
+        }
+    }
+
+}
---
+++ src/Foundation/ContentManagement/Repositories/ItemManagement.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Glass.Mapper.Sc;
+using Hackathon.Boilerplate.Foundation.ContentManagement.Interfaces;
+using Hackathon.Boilerplate.Foundation.ContentManagement.Models.SaudiAirlines.Foundation.Core.BaseClasses;
+using Sitecore.SecurityModel;
+
+namespace Hackathon.Boilerplate.Foundation.ContentManagement.Repositories
+{
+    public class ItemManagement:IItemManagement
+    {
+        private readonly ISitecoreService _sitecoreService;
+
+        public ItemManagement(ISitecoreService sitecoreService)
+        {
+            _sitecoreService = sitecoreService;
+
+        }
+        public T CreateSitecoreItemUsingParentPath<T>(T child, string parentPath) where T : GlassBase
+        {
+            // Check if the child or the parentPath is null 
+            if (child == null)
+                throw new ArgumentNullException(nameof(child));
+            if (string.IsNullOrWhiteSpace(parentPath))
+                throw new ArgumentNullException(nameof(parentPath));
+
+            //Get the parent item using the parent model from Sitecore
+            var parentItem = _sitecoreService.GetItem<GlassParent>(
+                new GetItemByPathOptions {Path = parentPath});
+
+            if (parentItem == null)
+                throw new ArgumentNullException($"ParentItem is null for parentPath of '{parentPath}'");
+
+            using (new SecurityDisabler())
+            {
+
+                return _sitecoreService.CreateItem<T>(
+                    new CreateByModelOptions {Model = child, Parent = parentItem});
+
+            }
+        }
+
+        public T CreateSitecoreItem<T>(T child, string parentId) where T : GlassBase
+        {
+            ///// Check if the child or the parentPath is null 
+            if (child == null)
+                throw new ArgumentNullException(nameof(child));
+            if (string.IsNullOrWhiteSpace(parentId))
+                throw new ArgumentNullException(nameof(parentId));
+            Guid id;
+            Guid.TryParse(parentId, out id);
+            if (id == null || id == Guid.Empty)
+            {
+                Sitecore.Diagnostics.Log.Error($"Error getting item by id:: provided id = {parentId} ",this);
+                return null;
+            }
+            //Get the parent item using the parent model from Sitecore
+            var parentItem = _sitecoreService.GetItem<GlassParent>(
+                new GetItemByIdOptions { Id = id });
+
+            if (parentItem == null)
+                throw new ArgumentNullException($"ParentItem is null for parentId of '{parentId}'");
+
+            using (new SecurityDisabler())
+            {
+
+                return _sitecoreService.CreateItem<T>(
+                    new CreateByModelOptions { Model = child, Parent = parentItem });
+
+            }
+        }
+    }
+}
